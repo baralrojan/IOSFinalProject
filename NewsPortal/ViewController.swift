72ced806eecdb0c8ffa9f80cd131b29ea767d295
Rojan <baralrojan088@gmail.com>
2023-03-11 16:57:07 -0800

    Initial Commit

---
+++ NewsPortal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsPortal
+//
+//  Created by user231854 on 3/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
